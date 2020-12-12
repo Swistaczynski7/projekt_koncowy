4e3a5e72ee1327a8c1e12f81773b1f4e59569e6c
Maciek Łazowski <rangerestel@gmail.com>
2020-12-12 11:08:54 +0100

    preparing to tests

---
+++ src/main/java/testingUtils/BaseDriver.java
@@ -0,0 +1,12 @@
+package testingUtils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseDriver {
+    public static WebDriver setDriver(){
+        System.setProperty("webdriver.chrome.driver","chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
+}
